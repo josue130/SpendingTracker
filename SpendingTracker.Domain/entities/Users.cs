34ec0a528ca6d5252275ffdabe6f2c780435cd3a
josue130 <josuelopez67i90@gmail.com>
2024-10-31 17:25:56 -0600

    Agregar archivos de proyecto.

---
+++ SpendingTracker.Domain/entities/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Domain.entities
+{
+    public class Users
+    {
+        public Guid Id { get; set; }
+        public string FullNmae { get; set; } = String.Empty;
+        public string UserName { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+    }
+}
---
+++ SpendingTracker.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpendingTracker.Domain.entities;
+
+
+namespace SpendingTracker.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Users> users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+          
+        }
+    }
+}
