31540bade42a42520e9e267ae36853f6297f0579
josue130 <josuelopez67i90@gmail.com>
2024-10-31 20:45:56 -0600

    feat(entities) : add Account entitie

---
+++ SpendingTracker.Domain/entities/Accounts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Domain.entities
+{
+    public class Accounts
+    {
+        public Guid Id { get; set; }
+        public string AccountName { get; set; } = string.Empty;
+        public double Amount { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+        public Accounts(Guid id, string accountName, double amount, string description)
+        {
+            Id = id;
+            AccountName = accountName;
+            Amount = amount;
+            Description = description;
+        }
+        public static Accounts Create(string accountName, double amount , string description)
+        {
+            Guid id = Guid.NewGuid();
+
+            return new Accounts(id, accountName, amount, description);
+        }
+        private Accounts()
+        {
+            
+        }
+    }
+}
