ec71fdd687d409c40309fd03e5e72123bb9fb86e
josue130 <josuelopez67i90@gmail.com>
2024-10-31 22:12:36 -0600

    feat(application): add DTO's

---
+++ SpendingTracker.Application/Common/Dto/RegisterRequestDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SpendingTracker.Application.Common.Dto
+{
+    public class RegisterRequestDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SpendingTracker.Application/Common/Dto/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Application.Common.Dto
+{
+    public class UserDto
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
