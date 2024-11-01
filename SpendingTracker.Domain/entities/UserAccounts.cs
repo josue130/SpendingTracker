23616b39d730cf9eb4e502acc2adb9b4aac20c86
josue130 <josuelopez67i90@gmail.com>
2024-10-31 20:46:07 -0600

    feat(entities) : add UserAccounts entitie

---
+++ SpendingTracker.Domain/entities/UserAccounts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Domain.entities
+{
+    public class UserAccounts
+    {
+        public Guid Id { get; set; }
+        public Guid AccountId { get; set; }
+        [ForeignKey("AccountId")]
+        public virtual Accounts Accounts { get; set; } = null!;
+        public Guid UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual Users Users { get; set; } = null!;
+    }
+}
