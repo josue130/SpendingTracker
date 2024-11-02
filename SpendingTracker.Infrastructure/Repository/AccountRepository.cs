d671e3c51fd3ae940bdc8894a188ba7ccb91975b
josue130 <josuelopez67i90@gmail.com>
2024-11-01 22:03:51 -0600

    feat(Infrastructure): add AccountRepository

---
+++ SpendingTracker.Infrastructure/Repository/AccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using SpendingTracker.Application.Common.Interface;
+using SpendingTracker.Domain.Entities;
+using SpendingTracker.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Infrastructure.Repository
+{
+    public class AccountRepository : Repository<Accounts>, IAccountRepository
+    {
+        public AccountRepository(AppDbContext db) : base(db)
+        {
+        }
+    }
+}
