7b392c68d51b2c406dc23526843a3904b19766d7
josue130 <josuelopez67i90@gmail.com>
2024-11-01 22:02:26 -0600

    feat(application): add AccountsDto

---
+++ SpendingTracker.Application/Common/Dto/AccountsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Application.Common.Dto
+{
+    public class AccountsDto
+    {
+        public Guid Id { get; set; }
+        public string AccountName { get; set; } = string.Empty;
+        public double Amount { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
