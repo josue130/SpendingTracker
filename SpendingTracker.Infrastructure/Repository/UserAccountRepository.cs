e8e1d1d46b513069b4b8ec93b3b2235b891710b2
josue130 <josuelopez67i90@gmail.com>
2024-11-02 17:43:50 -0600

    refactor(infrastructure): update UserAccountRepository

--- SpendingTracker.Infrastructure/Repository/UserAccountRepository.cs
+++ SpendingTracker.Infrastructure/Repository/UserAccountRepository.cs
@@ -1,4 +1,6 @@
-﻿using SpendingTracker.Application.Common.Interface;
+﻿using Microsoft.EntityFrameworkCore;
+using SpendingTracker.Application.Common.Dto;
+using SpendingTracker.Application.Common.Interface;
 using SpendingTracker.Domain.Entities;
 using SpendingTracker.Infrastructure.Data;
 using System;
@@ -11,8 +13,23 @@ namespace SpendingTracker.Infrastructure.Repository
 {
     public class UserAccountRepository : Repository<UserAccounts>, IUserAccountRepository
     {
+        private readonly AppDbContext _db;
         public UserAccountRepository(AppDbContext db) : base(db)
         {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<AccountsDto>> GetUserAccounts(Guid userId)
+        {
+            return await _db.userAccounts
+                .AsNoTrackingWithIdentityResolution()
+                .Where(ua=>ua.UserId == userId)
+                .Select(ua=> new AccountsDto {
+                    Id = ua.AccountId,
+                    AccountName = ua.Accounts.AccountName,
+                    Description = ua.Accounts.Description,
+                    Amount = ua.Accounts.Amount
+                }).ToListAsync();
         }
     }
 }
