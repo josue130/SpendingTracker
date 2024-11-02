d035bc5534ed86384db1c317aa97dbaeea49389b
josue130 <josuelopez67i90@gmail.com>
2024-11-01 22:01:58 -0600

    feat(application): add IUserAccountRepository

--- SpendingTracker.Application/Common/Interface/IUnitOfWork.cs
+++ SpendingTracker.Application/Common/Interface/IUnitOfWork.cs
@@ -5,6 +5,8 @@ namespace SpendingTracker.Application.Common.Interface
     public interface IUnitOfWork
     {
         IAuthRepository auth { get; }
+        IAccountRepository accounts { get; }
+        IUserAccountRepository userAccounts { get; }
         Task Save();
     }
 }
