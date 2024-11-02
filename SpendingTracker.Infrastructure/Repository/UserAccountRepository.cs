e04ef89b5f672cc3ed1133d6830182c8604c8f26
josue130 <josuelopez67i90@gmail.com>
2024-11-01 22:04:17 -0600

    feat(infrastructure): add UserAccountRepository

---
+++ SpendingTracker.Infrastructure/Repository/UserAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using SpendingTracker.Application.Common.Interface;
+using SpendingTracker.Domain.Entities;
+using SpendingTracker.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Infrastructure.Repository
+{
+    public class UserAccountRepository : Repository<UserAccounts>, IUserAccountRepository
+    {
+        public UserAccountRepository(AppDbContext db) : base(db)
+        {
+        }
+    }
+}
