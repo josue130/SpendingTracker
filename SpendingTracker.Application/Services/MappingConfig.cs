a02c8c46bb8b16998fa77b95b462e6bd37819681
josue130 <josuelopez67i90@gmail.com>
2024-11-02 17:44:45 -0600

    feat(application) add  mapper

---
+++ SpendingTracker.Application/Services/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SpendingTracker.Application.Common.Dto;
+using SpendingTracker.Domain.Entities;
+
+namespace SpendingTracker.Application.Services
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Accounts, AccountsDto>().ReverseMap();
+
+
+            });
+            return mappingConfig;
+        }
+    }
+}
