30c638cdf3166a94c26baeefc10ada11f2c17b90
josue130 <josuelopez67i90@gmail.com>
2024-11-02 17:42:39 -0600

    feat(application): add IUserRepository

--- SpendingTracker.Application/Common/Interface/IUserAccountRepository.cs
+++ SpendingTracker.Application/Common/Interface/IUserAccountRepository.cs
@@ -1,4 +1,5 @@
-﻿using SpendingTracker.Domain.Entities;
+﻿using SpendingTracker.Application.Common.Dto;
+using SpendingTracker.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,6 @@ namespace SpendingTracker.Application.Common.Interface
 {
     public interface IUserAccountRepository : IRepository<UserAccounts>
     {
+        Task<IEnumerable<AccountsDto>> GetUserAccounts(Guid userId);
     }
 }
