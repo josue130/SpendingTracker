85a5699715f0e1752a230a7ca00fcae3fa31e8d6
josue130 <josuelopez67i90@gmail.com>
2024-11-01 22:02:46 -0600

    feat(application): add AccountError

---
+++ SpendingTracker.Application/Errors/AccountsError.cs
@@ -0,0 +1,19 @@
+﻿using SpendingTracker.Application.Common.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Application.Errors
+{
+    public static class AccountsError
+    {
+        public static readonly Error InvalidInputs = new Error(
+            "Validation", "Inputs cannot be empty");
+
+        public static readonly Error InvalidAmount = new Error(
+            "Validation", "Amount must be greater than 0");
+
+    }
+}
