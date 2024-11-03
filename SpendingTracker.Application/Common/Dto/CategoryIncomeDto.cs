56cb014f97255e7ce4bf64813290d7ec55b16083
josue130 <josuelopez67i90@gmail.com>
2024-11-03 16:02:34 -0600

    feat(application) add CategoryIncomeDto

---
+++ SpendingTracker.Application/Common/Dto/CategoryIncomeDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Application.Common.Dto
+{
+    public class CategoryIncomeDto
+    {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; }
+        public string Color { get; set; }
+        public string Icon { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
