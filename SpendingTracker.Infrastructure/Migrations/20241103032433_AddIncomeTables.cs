f978a2b7c8497ad2cdb21684b569748832d76f49
josue130 <josuelopez67i90@gmail.com>
2024-11-02 21:29:26 -0600

    feat(infrastructure): add migrations

---
+++ SpendingTracker.Infrastructure/Migrations/20241103032433_AddIncomeTables.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpendingTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIncomeTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categoryIncome",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Icon = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categoryIncome", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "income",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Amount = table.Column<double>(type: "float", nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_income", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_income_categoryIncome_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "categoryIncome",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userIncome",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IncomeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AccountId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userIncome", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_userIncome_accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_userIncome_income_IncomeId",
+                        column: x => x.IncomeId,
+                        principalTable: "income",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_userIncome_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_income_CategoryId",
+                table: "income",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userIncome_AccountId",
+                table: "userIncome",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userIncome_IncomeId",
+                table: "userIncome",
+                column: "IncomeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userIncome_UserId",
+                table: "userIncome",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "userIncome");
+
+            migrationBuilder.DropTable(
+                name: "income");
+
+            migrationBuilder.DropTable(
+                name: "categoryIncome");
+        }
+    }
+}
