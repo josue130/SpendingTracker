5f69bd09a75b971c87ddeae2b614c43eb572691c
josue130 <josuelopez67i90@gmail.com>
2024-11-02 21:28:41 -0600

    feat(domain): add Income tables

---
+++ SpendingTracker.Domain/Entities/CategoryIncome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Domain.Entities
+{
+    public class CategoryIncome
+    {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; }
+        public string Color { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ SpendingTracker.Domain/Entities/Income.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Domain.Entities
+{
+    public class Income
+    {
+        public Guid Id { get; set; }
+        public double Amount { get; set; }
+        public Guid CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual CategoryIncome CategoryIncome { get; set; } = null!;
+        public DateTime Date { get; set; }
+    }
+}
