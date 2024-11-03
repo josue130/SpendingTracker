f75da18127ce3eaf057fab93708542e748c82d83
josue130 <josuelopez67i90@gmail.com>
2024-11-02 21:44:15 -0600

    feat(repository): add income respositories

---
+++ SpendingTracker.Application/Common/Interface/IIncomeRepository.cs
@@ -0,0 +1,13 @@
+﻿using SpendingTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Application.Common.Interface
+{
+    public interface IIncomeRepository : IRepository<Income>
+    {
+    }
+}
---
+++ SpendingTracker.Infrastructure/Repository/IncomeRepository.cs
@@ -0,0 +1,18 @@
+﻿using SpendingTracker.Application.Common.Interface;
+using SpendingTracker.Domain.Entities;
+using SpendingTracker.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Infrastructure.Repository
+{
+    public class IncomeRepository : Repository<Income>, IIncomeRepository
+    {
+        public IncomeRepository(AppDbContext db) : base(db)
+        {
+        }
+    }
+}
