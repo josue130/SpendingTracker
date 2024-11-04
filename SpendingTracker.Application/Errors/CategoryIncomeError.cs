03da39a4e2bce12cce83423b8827a69e6bdd5190
josue130 <josuelopez67i90@gmail.com>
2024-11-03 18:54:19 -0600

    feat(application): Add CategoryIncomeError

---
+++ SpendingTracker.Application/Errors/CategoryIncomeError.cs
@@ -0,0 +1,17 @@
+﻿using SpendingTracker.Application.Common.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Application.Errors
+{
+    public class CategoryIncomeError
+    {
+        public static readonly Error CategorytNotFound = new Error(
+            "AccountNotFound", "The category does not exist");
+        public static readonly Error InvalidInputs = new Error(
+            "Validation", "Inputs cannot be empty");
+    }
+}
