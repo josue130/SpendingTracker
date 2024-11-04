dc1b6e6d50041c2a40d0f2848211a3d951779c04
josue130 <josuelopez67i90@gmail.com>
2024-11-04 17:49:33 -0600

    Feat(application): add CategoryExpenseError

---
+++ SpendingTracker.Application/Errors/CategoryExpenseError.cs
@@ -0,0 +1,17 @@
+﻿using SpendingTracker.Application.Common.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Application.Errors
+{
+    public class CategoryExpenseError
+    {
+        public static readonly Error CategoryNotFound = new Error(
+            "AccountNotFound", "The category does not exist");
+        public static readonly Error InvalidInputs = new Error(
+            "Validation", "Inputs cannot be empty");
+    }
+}
