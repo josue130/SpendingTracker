7a39b24e1e22bb5d79249bf92be5259f8f30d5b0
josue130 <josuelopez67i90@gmail.com>
2024-11-03 21:22:36 -0600

    feat(domain): add expense tables

---
+++ SpendingTracker.Domain/Entities/CategoryExpense.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Domain.Entities
+{
+    public class CategoryExpense
+    {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+        public Guid UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual Users Users { get; set; } = null!;
+    }
+}
---
+++ SpendingTracker.Domain/Entities/Expense.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Domain.Entities
+{
+    public class Expense
+    {
+        public Guid Id { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public Guid AccountId { get; set; }
+        [ForeignKey("AccountId")]
+        public virtual Accounts Accounts { get; set; } = null!;
+        public Guid CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual CategoryExpense CategoryExpense { get; set; } = null!;
+    }
+}
