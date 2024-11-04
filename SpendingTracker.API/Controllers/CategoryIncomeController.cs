a361c90b54381f44964c6d77f2249207bf199e8e
josue130 <josuelopez67i90@gmail.com>
2024-11-03 18:56:08 -0600

    feat(api): add CategoryIncomeController

---
+++ SpendingTracker.API/Controllers/CategoryIncomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpendingTracker.Application.Common.Dto;
+using SpendingTracker.Application.Services.IServices;
+
+namespace SpendingTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryIncomeController : ControllerBase
+    {
+        private readonly ICategoryIncomeService _categoryIncomeService;
+        public CategoryIncomeController(ICategoryIncomeService categoryIncomeService)
+        {
+            _categoryIncomeService = categoryIncomeService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var response = await _categoryIncomeService.GetCategories(User);
+            return Ok(response);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CategoryIncomeDto categoryIncomeDto)
+        {
+            var response = await _categoryIncomeService.CreateCategoryIncome(categoryIncomeDto, User);
+            if (response.IsFailure)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] CategoryIncomeDto categoryIncomeDto)
+        {
+            var response = await _categoryIncomeService.UpdateCategoryIncome(categoryIncomeDto, User);
+            if (response.IsFailure)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] Guid categoryId)
+        {
+            var response = await _categoryIncomeService.DeleteCategoryIncome(categoryId, User);
+            if (response.IsFailure)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
