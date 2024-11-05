42fe758980668c481b50ed9778f1d24c4f5af009
josue130 <josuelopez67i90@gmail.com>
2024-11-04 20:43:32 -0600

    Feat(domain): add MonthlyBalances table

---
+++ SpendingTracker.Domain/Entities/MonthlyBalances.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Domain.Entities
+{
+    public class MonthlyBalances
+    {
+        public Guid Id { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public double Balance { get; set; }
+        public Guid AccountId { get; set; }
+        [ForeignKey("AccountId")]
+        public virtual Accounts Accounts { get; set; } = null!;
+    }
+    
+}
