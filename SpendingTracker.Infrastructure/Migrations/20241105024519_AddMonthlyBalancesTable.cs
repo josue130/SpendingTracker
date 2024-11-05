dae58dc56281d0be0c7eaad31d0c974c64305017
josue130 <josuelopez67i90@gmail.com>
2024-11-04 21:25:10 -0600

    Feat(Infrastructure): add migrations

---
+++ SpendingTracker.Infrastructure/Migrations/20241105024519_AddMonthlyBalancesTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpendingTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMonthlyBalancesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "income");
+
+            migrationBuilder.CreateTable(
+                name: "monthlyBalances",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    Balance = table.Column<double>(type: "float", nullable: false),
+                    AccountId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_monthlyBalances", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_monthlyBalances_accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_monthlyBalances_AccountId",
+                table: "monthlyBalances",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "monthlyBalances");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Amount",
+                table: "income",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
