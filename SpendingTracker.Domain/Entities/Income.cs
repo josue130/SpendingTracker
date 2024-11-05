e79cbfa18c7554f2cba814ac775916ce46ab7100
josue130 <josuelopez67i90@gmail.com>
2024-11-04 20:42:11 -0600

    Refactor(domain): update income table

--- SpendingTracker.Domain/Entities/Income.cs
+++ SpendingTracker.Domain/Entities/Income.cs
@@ -10,7 +10,6 @@ namespace SpendingTracker.Domain.Entities
     public class Income
     {
         public Guid Id { get; set; }
-        public double Amount { get; set; }
         public string Description { get; set; } = string.Empty;
         public DateTime Date { get; set; }
         public Guid AccountId { get; set; }
