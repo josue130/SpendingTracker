1e19e7f7d6b19c2dcfc4b7a60dc6def0bbd84488
josue130 <josuelopez67i90@gmail.com>
2024-11-05 21:04:40 -0600

    Feat(application): add IncomeDto

---
+++ SpendingTracker.Application/Common/Dto/IncomeDto.cs
@@ -0,0 +1,30 @@
+﻿using SpendingTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Application.Common.Dto
+{
+    public class IncomeDto
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El monto es obligatorio.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El monto debe ser mayor que cero.")]
+        public double Amount { get; set; }
+
+        [Required(ErrorMessage = "La fecha es obligatoria.")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "El Id de la cuenta es obligatorio.")]
+        public Guid AccountId { get; set; }
+
+        [Required(ErrorMessage = "El Id de la categoría es obligatorio.")]
+        public Guid CategoryId { get; set; }
+    }
+}
