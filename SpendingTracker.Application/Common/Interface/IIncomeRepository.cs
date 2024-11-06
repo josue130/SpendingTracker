5b157d9913684d9b8b4540c4a948f26a33698a91
josue130 <josuelopez67i90@gmail.com>
2024-11-05 21:04:23 -0600

    Refactor(respository): add new requests

--- SpendingTracker.Application/Common/Interface/IIncomeRepository.cs
+++ SpendingTracker.Application/Common/Interface/IIncomeRepository.cs
@@ -9,5 +9,9 @@ namespace SpendingTracker.Application.Common.Interface
 {
     public interface IIncomeRepository : IRepository<Income>
     {
+        Task<bool> CheckUserAccess(Guid incomeId, Guid userId);
+
+        Task<IEnumerable<Income>> GetIncomesByAccountId(Guid accountId, int month, int year);
+        void Update(Income income);
     }
 }
--- SpendingTracker.Infrastructure/Repository/IncomeRepository.cs
+++ SpendingTracker.Infrastructure/Repository/IncomeRepository.cs
@@ -1,4 +1,5 @@
-﻿using SpendingTracker.Application.Common.Interface;
+﻿using Microsoft.EntityFrameworkCore;
+using SpendingTracker.Application.Common.Interface;
 using SpendingTracker.Domain.Entities;
 using SpendingTracker.Infrastructure.Data;
 using System;
@@ -11,8 +12,40 @@ namespace SpendingTracker.Infrastructure.Repository
 {
     public class IncomeRepository : Repository<Income>, IIncomeRepository
     {
+        private readonly AppDbContext _db;
         public IncomeRepository(AppDbContext db) : base(db)
         {
+            _db = db;
+        }
+
+        public async Task<bool> CheckUserAccess(Guid incomeId, Guid userId)
+        {
+            var income = await _db.income
+                .AsNoTracking()
+                .Where(i => i.Id == incomeId)
+                .Join(_db.userAccounts,
+                    i => i.AccountId,
+                    ua => ua.AccountId,
+                    (i, ua) => new { Income = i, UserAccount = ua })
+                .FirstOrDefaultAsync(x => x.UserAccount.UserId == userId);
+            if (income == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public async Task<IEnumerable<Income>> GetIncomesByAccountId(Guid accountId, int month, int year)
+        {
+            return await _db.income.Where(i => i.AccountId == accountId &&
+                i.Date.Year == year &&
+                i.Date.Month == month)
+                .ToListAsync();
+        }
+
+        public void Update(Income income)
+        {
+            _db.income.Update(income);
         }
     }
 }
