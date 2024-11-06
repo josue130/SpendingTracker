988b6f9786733ac48404338727e6e8f69514d2ec
josue130 <josuelopez67i90@gmail.com>
2024-11-05 21:03:12 -0600

    Refactor(infrastructure): add new migration

--- SpendingTracker.Infrastructure/Migrations/20241105212147_AddAmountToIncomeTable.Designer.cs
+++ SpendingTracker.Infrastructure/Migrations/20241105212147_AddAmountToIncomeTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpendingTracker.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using SpendingTracker.Infrastructure.Data;
 namespace SpendingTracker.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241105212147_AddAmountToIncomeTable")]
+    partial class AddAmountToIncomeTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
