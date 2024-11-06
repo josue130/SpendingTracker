1d146989a25d464dfa0e8c342b4fea798b68ad42
josue130 <josuelopez67i90@gmail.com>
2024-11-05 21:02:06 -0600

    Refactor(domain): update income entitie

--- SpendingTracker.Domain/Entities/Income.cs
+++ SpendingTracker.Domain/Entities/Income.cs
@@ -11,6 +11,7 @@ namespace SpendingTracker.Domain.Entities
     {
         public Guid Id { get; set; }
         public string Description { get; set; } = string.Empty;
+        public double Amount { get; set; }
         public DateTime Date { get; set; }
         public Guid AccountId { get; set; }
         [ForeignKey("AccountId")]
@@ -18,5 +19,33 @@ namespace SpendingTracker.Domain.Entities
         public Guid CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public virtual CategoryIncome CategoryIncome { get; set; } = null!;
+
+
+        public Income(Guid id, string description, double amount, DateTime date, Guid accountId, Guid categoryId)
+        {
+            Id = id;
+            Description = description;
+            Amount = amount;
+            Date = date;
+            AccountId = accountId;
+            CategoryId = categoryId;
+        }
+
+        public static Income Create(string description, double amount, DateTime date, Guid accountId, Guid categoryId)
+        {
+            return new Income
+            {
+                Id = Guid.NewGuid(),
+                Description = description,
+                Amount = amount,
+                Date = date,
+                AccountId = accountId,
+                CategoryId = categoryId
+            };
+        }
+        private Income()
+        {
+            
+        }
     }
 }
