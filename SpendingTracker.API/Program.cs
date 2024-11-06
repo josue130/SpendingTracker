2319f46477992ec7f5642b7398dc25eea6605443
josue130 <josuelopez67i90@gmail.com>
2024-11-05 21:06:50 -0600

    Feat(api): add IncomeService

--- SpendingTracker.API/Program.cs
+++ SpendingTracker.API/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<ICategoryIncomeService, CategoryIncomeService>();
 builder.Services.AddScoped<ICategoryExpenseService, CategoryExpenseService>();
+builder.Services.AddScoped<IIncomeService, IncomeService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(option =>
