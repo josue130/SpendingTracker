0f2be67bae7162deaa77c2881900afa0fbebd13a
josue130 <josuelopez67i90@gmail.com>
2024-11-06 15:08:14 -0600

    Feat(application): add ExpenseError

---
+++ SpendingTracker.Application/Errors/ExpenseError.cs
@@ -0,0 +1,15 @@
+﻿using SpendingTracker.Application.Common.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Application.Errors
+{
+    public class ExpenseError
+    {
+        public static readonly Error ExpenseNotFound = new Error(
+            "ExpenseNotFound", "The expense does not exist");
+    }
+}
