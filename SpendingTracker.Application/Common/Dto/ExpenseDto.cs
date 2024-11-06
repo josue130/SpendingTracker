94518892027b8d9f8713f9f82119b58bf74fa014
josue130 <josuelopez67i90@gmail.com>
2024-11-06 15:08:41 -0600

    Feat(application): Add ExpenseDto

---
+++ SpendingTracker.Application/Common/Dto/ExpenseDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SpendingTracker.Application.Common.Dto
+{
+    public class ExpenseDto
+    {
+        public Guid Id { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public Guid AccountId { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
