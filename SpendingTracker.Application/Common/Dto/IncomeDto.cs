e46e86643c314a21c379a4cfee5f11b669b99eb2
josue130 <josuelopez67i90@gmail.com>
2024-11-07 21:12:19 -0600

    Feat(application) : add ExceptionResponse

--- SpendingTracker.Application/Common/Dto/IncomeDto.cs
+++ SpendingTracker.Application/Common/Dto/IncomeDto.cs
@@ -13,18 +13,9 @@ namespace SpendingTracker.Application.Common.Dto
     {
         public Guid Id { get; set; }
         public string Description { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "El monto es obligatorio.")]
-        [Range(0.01, double.MaxValue, ErrorMessage = "El monto debe ser mayor que cero.")]
         public double Amount { get; set; }
-
-        [Required(ErrorMessage = "La fecha es obligatoria.")]
         public DateTime Date { get; set; }
-
-        [Required(ErrorMessage = "El Id de la cuenta es obligatorio.")]
         public Guid AccountId { get; set; }
-
-        [Required(ErrorMessage = "El Id de la categoría es obligatorio.")]
         public Guid CategoryId { get; set; }
     }
 }
