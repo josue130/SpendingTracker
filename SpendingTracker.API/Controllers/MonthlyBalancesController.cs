c66b057e6a39ae7f294d2e415060fe233413596e
josue130 <josuelopez67i90@gmail.com>
2024-11-07 21:46:57 -0600

    Feat(api): add MonthlyBalancesController

---
+++ SpendingTracker.API/Controllers/MonthlyBalancesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using SpendingTracker.Application.Services;
+using SpendingTracker.Application.Services.IServices;
+
+namespace SpendingTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonthlyBalancesController : ControllerBase
+    {
+        private readonly IMonthlyBalancesService _monthlyBalancesService;
+
+        public MonthlyBalancesController(IMonthlyBalancesService monthlyBalancesService)
+        {
+            _monthlyBalancesService = monthlyBalancesService;
+        }
+        [HttpGet("{accountId:guid}")]
+        public async Task<IActionResult> Get(Guid accountId)
+        {
+            var response = await _monthlyBalancesService.GetMonthlyBalance(accountId, User);
+            if (response.IsFailure)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
