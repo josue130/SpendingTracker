2798d30391227223f07eed92ce5372f1ee785eb8
josue130 <josuelopez67i90@gmail.com>
2024-11-25 15:05:47 -0600

    feat(api): add CurrencyController

---
+++ SpendingTracker.API/Controllers/CurrencyController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SpendingTracker.Application.Common.Interface;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpendingTracker.API.Controllers
+{
+    [Route("api/currency")]
+    [ApiController]
+    [Authorize]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly ICurrencyService _currencyService;
+        public CurrencyController(ICurrencyService currencyService)
+        {
+            _currencyService = currencyService;
+        }
+
+        [HttpGet("currencies")]
+        public IActionResult GetCurrencies()
+        {
+            var currencies = _currencyService.Currencies();
+            return Ok(currencies);
+        }
+
+        [HttpGet("latest")]
+        public IActionResult GetLatestRates([FromQuery] string baseCurrency, [FromQuery] string currencies)
+        {
+            var latestRates = _currencyService.LatestCurrency(baseCurrency, currencies);
+            return Ok(latestRates);
+        }
+    }
+}
