5b68f47e37a66d375952306225911816e3b7506f
josue130 <josuelopez67i90@gmail.com>
2024-11-25 15:52:28 -0600

    feat(infrastructure): add RedisCacheService

---
+++ SpendingTracker.Infrastructure/Redis/RedisCacheService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using SpendingTracker.Application.Common.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Infrastructure.Redis
+{
+    public class RedisCacheService : IRedisCacheService
+    {
+        private readonly IDistributedCache? _cache;
+
+        public RedisCacheService(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+        public T? GetData<T>(string key)
+        {
+            var data = _cache?.GetString(key);
+            
+            if(data == null) return default(T);
+
+            return JsonSerializer.Deserialize<T>(data);
+        }
+
+        public void SetData<T>(string key, T data)
+        {
+            var options = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromDays(1)
+            };
+
+            _cache?.SetString(key,JsonSerializer.Serialize(data),options);
+        }
+    }
+}
