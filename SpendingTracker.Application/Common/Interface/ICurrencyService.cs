37a21a54ff55039635fa25539bcf13c815b0b44e
josue130 <josuelopez67i90@gmail.com>
2024-11-25 15:04:08 -0600

    feat(application): add CurrencyService Interface

---
+++ SpendingTracker.Application/Common/Interface/ICurrencyService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Application.Common.Interface
+{
+    public interface ICurrencyService
+    {
+        string Currencies();
+
+        string LatestCurrency(string baseCurrency,string currencies);
+    }
+}
