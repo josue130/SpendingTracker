b59e8b3788999eae72a6e1011c0bdcdfdd973ed9
josue130 <josuelopez67i90@gmail.com>
2024-11-26 21:59:43 -0600

    feat(application): add JWTError

---
+++ SpendingTracker.Application/Errors/JWTError.cs
@@ -0,0 +1,15 @@
+﻿using SpendingTracker.Application.Common.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingTracker.Application.Errors
+{
+    public class JWTError
+    {
+        public static readonly Error JwtTokenInvalid = new Error(
+            "Token", "Jwt token invalid");
+    }
+}
